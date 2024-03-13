107c50fb1e2a88dfc20f39e1052207b1ab2ba04b
Hamzin Daulet <qdis009@gmail.com>
2024-03-13 23:18:57 +0500

    add controller, consumer and producer

---
+++ src/main/java/kz/example/rabbitMQ/controller/MessageController.java
@@ -0,0 +1,23 @@
+package kz.example.rabbitMQ.controller;
+
+import kz.example.rabbitMQ.produser.RabbitMQProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class MessageController {
+    private final RabbitMQProducer producer;
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
+        producer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to RabbitMQ");
+    }
+
+}
---
+++ src/main/java/kz/example/rabbitMQ/produser/RabbitMQProducer.java
@@ -0,0 +1,24 @@
+package kz.example.rabbitMQ.produser;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitMQProducer {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+        LOGGER.info(String.format("Message sent -> %s", message));
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
+    }
+}
